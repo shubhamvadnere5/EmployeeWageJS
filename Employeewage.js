2975d313656e57aa5167fca56d9a6af6c8dd1af5
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-25 11:46:31 +0530

    [ShubhamPV] UC1 Employee is absent or persent

---
+++ Employeewage.js
@@ -0,0 +1,19 @@
+//UC1//
+const IS_ABSENT=0;
+
+
+//using random function to identify employee is present or not
+{
+    let empCheck=Math.floor(Math.random()*10) % 2;
+
+//using programming construct that is selection statement
+     if(empCheck==IS_ABSENT)
+     {
+         console.log("UC1: employee absent");
+     }
+     else
+     {
+         console.log("UC1: employee present");
+     }
+
+
