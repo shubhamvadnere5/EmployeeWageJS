aeaa800436d12c05a5fcfd5e7eb47c14ad6bd0f1
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-25 13:03:11 +0530

    [ShubhamPV] Employee Hours and Days Condition

--- Employeewage.js
+++ Employeewage.js
@@ -1,13 +1,14 @@
-//UC3 Employee Function//
+//UC4 //
 
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HRS=4;
 const FULL_TIME_HRS=8;
 const WAGE_PER_HR=100;
+const MAX_NUMBER_DAYS=20;
+const MAX_HOURS=160;
 
-let emphrs=4;
-empCheck=Math.floor(Math.random ()) * 3;
+let emphrs=0;
 function getemphrs(empCheck)
 {
     switch(empCheck)
@@ -19,9 +20,17 @@ function getemphrs(empCheck)
         emphrs=FULL_TIME_HRS;
         break;
      default:
-         return 4; 
+         return 0; 
 }
 }
-emphrs=getemphrs(empCheck);
+let days=0;
+let maxhrs=0;
+while(days<MAX_NUMBER_DAYS && emphrs<=160)
+{
+    let empCheck=Math.floor(Math.random()*10)%3;
+    emphrs+=getemphrs(empCheck);
+    days=days+1;
+}
+
 let empwage = emphrs*WAGE_PER_HR;
-console.log("UC3 Emp Daily wage is:" +empwage);
+console.log("UC5 Total Emp wage is:" +emphrs +"Hours "+" Working for"+days+" days "+"is :"+empwage);
